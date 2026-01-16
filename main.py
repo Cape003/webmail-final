5104d67fe32a81a47112d459c2a50dc908bdf349
Cape003 <tristan@durieu.net>
2026-01-17 00:35:29 +0100

    Enable allow_unsafe_werkzeug for socketio.run

--- main.py
+++ main.py
@@ -167,6 +167,7 @@ def on_m(d):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+
 
 
